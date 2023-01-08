e5ce7a3ca2118aaab52bf483e04d93a9e118cf98
gokhancuryan <119778787+gokhancuryan@users.noreply.github.com>
2023-01-08 21:13:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace metotOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //out parametreler
+            string sayi = "999";
+          bool sonuc = int.TryParse(sayi,out int outSayi);
+          if ( sonuc)
+          {
+            Console.WriteLine("basarili");
+            Console.WriteLine(outSayi);
+          }
+          else{
+            Console.WriteLine("Basarisiz");
+          }
+
+          Metotlar instance = new Metotlar();
+          instance.Topla(2,3, out int toplamsonuc);
+          Console.WriteLine(toplamsonuc);
+
+
+          // Metot Overloading
+            int ifade = 959;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Gökhan","Çüryan");
+
+        }
+    }
+
+    class Metotlar
+    {
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam = a+b;
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(string veri1, string veri2)
+        {
+            Console.WriteLine(veri1 + veri2);
+        }
+    }
+}
